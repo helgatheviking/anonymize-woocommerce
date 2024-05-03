05590200000396d6f7f897e664968d4d86e41d88
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2024-05-03 14:38:34 -0400

    Override setting to force delete download data

--- includes/AnonymizeCustomerProcessor.php
+++ includes/AnonymizeCustomerProcessor.php
@@ -121,6 +121,7 @@ class AnonymizeCustomerProcessor implements BatchProcessorInterface, RegisterHoo
 		// IMPORTANT! Don't send email and password change notifications.
 		\add_filter( 'send_email_change_email', '__return_false' );
 		\add_filter( 'send_password_change_email', '__return_false' );
+		\add_filter( 'woocommerce_privacy_erase_download_personal_data', '__return_true' );
 
 		if ( empty( $batch ) ) {
 			return;
